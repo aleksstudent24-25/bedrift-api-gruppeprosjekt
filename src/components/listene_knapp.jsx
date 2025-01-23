266d17db58d6a64a1e9f1e6a2a13edcd803390ef
Lillian24sfj <lillian24sfj@gmail.com>
2025-01-23 10:03:38 +0100

    fetchet

--- src/components/listene_knapp.jsx
+++ src/components/listene_knapp.jsx
@@ -1,4 +1,4 @@
-import kommuner from "../Data/kommuner";
+import KommuneDropdown from "../data/fetch_kommuner";
 
 
 export default function RullNed(){
@@ -7,11 +7,7 @@ export default function RullNed(){
 
   return (
     <div>
-    <select className="kommuner">
-      {kommuner.map((kommune) => (
-        <option key={kommune}>{kommune}</option>
-      ))}
-    </select>
+    <KommuneDropdown />
     <input type="number" min="1900" max="2025" step="1" value="2016" />
   <button>Search</button>
   </div>
