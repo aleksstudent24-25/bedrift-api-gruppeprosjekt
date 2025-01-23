f6ae0de6c1c97732aae0895d260d05d8ebcd0dc2
the-bug-lover <aleksander24sfj@gmail.com>
2025-01-23 10:34:19 +0100

    test

--- src/components/listene_knapp.jsx
+++ src/components/listene_knapp.jsx
@@ -1,4 +1,3 @@
-
 import KommuneDropdown from "../data/fetch_kommuner";
 import { useState } from "react";
 import kommuner from "../Data/kommuner";
@@ -9,18 +8,25 @@ export default function RullNed() {
     kommune: "Oslo",
     year: "2016",
   });
-  return <>
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...allValues, [name]: value });
+  };
+
+  return (
+    <>
       <h2 class="title"> Bedrift Søker </h2>
-       <KommuneDropdown />
-        <input
-          name="year"
-          type="number"
-          min="1900"
-          max="2025"
-          step="1"
-          value={allValues.year}
-          onChange={handleChange}
-        />
+      <KommuneDropdown />
+      <input
+        name="year"
+        type="number"
+        min="1900"
+        max="2025"
+        step="1"
+        value={allValues.year}
+        onChange={handleChange}
+      />
       <div className="result">
         <ShowData kommune={allValues.kommune} year={allValues.year}></ShowData>
       </div>
